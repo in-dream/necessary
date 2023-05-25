508d4270d4d59b05faf806df4c18a9443509a3f8
NaCoLiu <naco@nco.im>
2023-05-25 23:27:39 +0800

    feature(qrcode): add post qrcode share

---
+++ src/vue/QrcodeShare.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import '../styles/global.scss';
+import { useQRCode } from '@vueuse/integrations/useQRCode'
+
+const qrcode = useQRCode('https://baidu.com', {
+    rendererOpts: {
+        quality: 1,
+    }
+})
+</script>
+<template>
+    <div class="w-[350px] rounded overflow-hidden shadow relative" id="qrcodeshare">
+        <img src="../assets/images/qrcode.jpg" alt="header" class="w-full h-full object-cover " />
+        <div class="p-3 font-bold">
+            <div class="text-2xl">
+                寻你。
+            </div>
+            <div class="text-sm pt-3">那天雾盘踞在山谷中，天青色的湖。仅此而已，什么都看不清。</div>
+            <div class="share-code py-2">
+                <img :src=qrcode alt="qrcode" class="w-28" />
+            </div>
+        </div>
+    </div>
+</template>
